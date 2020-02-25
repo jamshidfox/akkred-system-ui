60c5bd2ece8593629167614ae367809907cd5ac6
jusss <spectral.ball@gmail.com>
2020-02-26 00:30:21 +0500

    login page refactoring

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -2,23 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch } from 'react-redux'
 import { hotelFetchList } from '../Register/actions'
-import * as ROUTES from '../../constants/routes'
 import Login from './components/Login'
 import { loginAction, userInfoFetch } from './actions'
+import * as ROUTES from '~/constants/routes'
+import { mapResponseToFormError } from '~/utils/form'
 
 const LoginContainer = props => {
   const dispatch = useDispatch()
-  const onLogin = (data) => {
-    return dispatch(loginAction(data))
+  const onLogin = values => {
+    return dispatch(loginAction(values))
       .then(({ value }) => dispatch(userInfoFetch(value.token)))
       .then(() => props.history.push(ROUTES.SETTINGS_HOTEL_URL))
       .then(() => dispatch(hotelFetchList()))
+      .catch(mapResponseToFormError)
   }
   return (
     <Login onLogin={onLogin} />
   )
 }
+
 LoginContainer.propTypes = {
   history: PropTypes.object
 }
+
 export default LoginContainer
---
+++ src/containers/Login/components/LoginFields.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Field, InputField } from '~/components/FormField'
+import { MediumButton } from '~/components/UI'
+import { Box as BoxUI } from '~/components/StyledElems'
+
+const Box = styled(BoxUI)`
+  box-shadow: 0 16px 24px rgba(8, 35, 48, 0.16);
+  width: 430px;
+  padding: 56px;
+`
+
+const Fields = styled('div')`
+  position:relative;
+`
+
+const InputWrap = styled('div')`
+  &:not(:first-child) {
+    margin-top: 40px;
+  }
+`
+
+const Actions = styled('div')`
+  align-items: center;
+  display:flex;
+  justify-content: space-between;
+  margin-top: 40px;
+`
+
+const Forgot = styled.div`
+  color: ${props => props.theme.color.basic.default};
+  cursor:pointer;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+`
+
+const SubmitButton = styled(MediumButton)`
+  width: 120px;
+`
+
+const SubmitError = styled('div')`
+  color: ${props => props.theme.color.danger.default};
+  font-size: 12px;
+  text-align: left;
+  margin: 8px 0 -22px;
+`
+
+const LoginFields = props => {
+  const { submitError } = props
+
+  return (
+    <Box>
+      <Fields>
+        <InputWrap>
+          <Field
+            name={'username'}
+            component={InputField}
+            placeholder={'Введите логин'}
+          />
+        </InputWrap>
+        <InputWrap>
+          <Field
+            name={'password'}
+            component={InputField}
+            placeholder={'Введите пароль'}
+          />
+        </InputWrap>
+
+        {submitError && (
+          <SubmitError>{submitError}</SubmitError>
+        )}
+      </Fields>
+
+      <Actions>
+        <Forgot>Забыли пароль?</Forgot>
+        <SubmitButton type={'submit'}>Войти</SubmitButton>
+      </Actions>
+    </Box>
+  )
+}
+
+LoginFields.propTypes = {
+  submitError: PropTypes.string,
+}
+
+export default LoginFields
