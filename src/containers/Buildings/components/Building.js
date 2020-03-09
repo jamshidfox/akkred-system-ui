38d03175c5e3f9230f110b5636377744f4159a9e
Rafael <rafael@swnuz.com>
2020-03-09 13:54:24 +0500

    containers

---
+++ src/containers/Buildings/components/Building.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { prop, isEmpty } from 'ramda'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Edit from 'images/edit.svg'
+import Trash from 'images/trash-2.svg'
+import { TableCol, Table, TableRow } from '../../../components/Table'
+import { PageTitle, MediumButton } from '../../../components/UI'
+import { Box } from '../../../components/StyledElems'
+import BuildingsCreateModal from './BuildingsCreateModal'
+import BuildingsDeleteModal from './BuildingsDeleteModal'
+
+const BoxUI = styled(Box)`
+  padding: 25px;
+`
+
+const linkStyle = {
+  color: '#FFF',
+  textDecoration: 'none'
+}
+const linkListStyle = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
+
+const BuildingsList = props => {
+  const { list, createModal, initialValues, deleteModal, deleteBuilding } = props
+  const [deleteItem, setDeleteItem] = useState({})
+  const data = prop('results', list)
+
+  const deleteItemModal = (id, name) => {
+    setDeleteItem({ id: id, name: name })
+    deleteModal.onOpen()
+  }
+
+  return (
+    <BoxUI>
+      <PageTitle name="Корпуса">
+        <MediumButton onClick={createModal.onOpen}>добавить</MediumButton>
+      </PageTitle>
+      <Table isEmpty={isEmpty(data)}>
+        <TableRow header={true} >
+          <TableCol span={6}>Название корпуса</TableCol>
+          <TableCol span={6}>Этажность</TableCol>
+          <TableCol span={5}>Количество номеров</TableCol>
+          <TableCol span={7} />
+        </TableRow>
+        {data.map((building, index) => (
+          <TableRow key={index}>
+            <TableCol span={6}>{building.name}</TableCol>
+            <TableCol span={6}>{building.floors.length}</TableCol>
+            <TableCol span={5}>{building.count}</TableCol>
+            <TableCol span={5} />
+            <TableCol span={2} style={linkListStyle}>
+              <Link style={linkStyle} to={''}><img src={Edit} alt="Edit" /></Link>
+              <span style={linkStyle} onClick={() => deleteItemModal(building.id, building.name)}><img
+                src={Trash} alt="Delete" /></span>
+            </TableCol>
+          </TableRow>
+        ))}
+      </Table>
+      <BuildingsCreateModal {...createModal} initialValues={initialValues} />
+      <BuildingsDeleteModal {...deleteModal} deleteItem={deleteItem} deleteBuilding={deleteBuilding} />
+    </BoxUI>
+  )
+}
+
+BuildingsList.propTypes = {
+  list: PropTypes.object,
+  createModal: PropTypes.object,
+  deleteModal: PropTypes.object,
+  deleteBuilding: PropTypes.object,
+  initialValues: PropTypes.object
+}
+
+export default BuildingsList
