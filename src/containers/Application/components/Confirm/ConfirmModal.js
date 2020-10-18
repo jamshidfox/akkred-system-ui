3575dd689613bf10efc0a0a76e07bb8a8e268526
Hasanboy <stainlessclash@gmail.com>
2020-10-19 00:45:06 +0500

    fix

--- src/containers/Application/components/Confirm/ConfirmModal.js
+++ src/containers/Application/components/Confirm/ConfirmModal.js
@@ -1,69 +1,21 @@
-
 import React from 'react'
-import { MediumButton } from '../../../../components/UI/Buttons'
-import { Modal } from '../../../../components/UI'
-import { Field, Form, InputField } from '../../../../components/FormField'
-import { FieldWrapper } from '../../../../components/StyledElems'
+import ConfirmStageOne from './ConfirmStages/ConfirmStageOne'
+import ConfirmStageTwo from './ConfirmStages/ConfirmStageTwo'
+import ConfirmStageThree from './ConfirmStages/ConfirmStageThree'
 
 const ConfirmDialog = ({ onClose, open, onSubmit, stage }) => {
-  const onSubmitSec = () => {
-
-  }
-
   switch (stage) {
-  // case 'stage_1':
-  //   return (
-  //     <Modal onClose={onClose} open={open} width={'400px'}>
-  //       <div style={{ textAlign: 'right' }}>
-  //         <MediumButton onClick={onSubmit}>Confirm stage_1</MediumButton>
-  //       </div>
-  //     </Modal>
-  //   )
+  case 'stage':
+    return (
+      <ConfirmStageOne onClose={onClose} onSubmit={onSubmit} open={open} />
+    )
+  case 'stage_':
+    return (
+      <ConfirmStageTwo onClose={onClose} onSubmit={onSubmit} open={open} />
+    )
   case 'stage_1':
     return (
-      <Modal onClose={onClose} open={open} width={'400px'}>
-        <Form
-          onSubmit={onSubmitSec}
-          render={({ handleSubmit }) => {
-            return (
-              <form onSubmit={handleSubmit}>
-                <FieldWrapper>
-                  <Field
-                    name="address"
-                    label="address"
-                    component={InputField}
-                  />
-                </FieldWrapper>
-                <FieldWrapper>
-                  <Field
-                    name="phoneNumber"
-                    label="phoneNumber"
-                    component={InputField}
-                  />
-                </FieldWrapper>
-                <FieldWrapper>
-                  <Field
-                    name="fullName"
-                    label="fullName"
-                    component={InputField}
-                  />
-                </FieldWrapper>
-                <div style={{ textAlign: 'right' }}>
-                  <MediumButton type="submit">Сохранить</MediumButton>
-                </div>
-              </form>
-            )
-          }}
-        />
-      </Modal>
-    )
-  case 'stage_3':
-    return (
-      <Modal onClose={onClose} open={open} width={'400px'}>
-        <div style={{ textAlign: 'right' }}>
-          <MediumButton onClick={onSubmit}>Confirm stage_3</MediumButton>
-        </div>
-      </Modal>
+      <ConfirmStageThree onClose={onClose} onSubmit={onSubmit} open={open} />
     )
 
   default:
