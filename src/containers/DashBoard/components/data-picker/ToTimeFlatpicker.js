ebcbad0470759bc5e07579652366dcc3d335e036
Jamshid <jamanu1910077@gmail.com>
2022-01-18 21:42:13 +0500

    Flatpicker styled

--- src/containers/DashBoard/components/data-picker/ToTimeFlatpicker.js
+++ src/containers/DashBoard/components/data-picker/ToTimeFlatpicker.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Fragment, useState } from 'react'
 import Flatpickr from 'react-flatpickr'
-import "flatpickr/dist/themes/material_green.css";
+import { Label } from 'reactstrap'
+import 'flatpickr/dist/themes/material_green.css'
 
-
-const PickerDefault = () => {
+const PickerDefault = (props) => {
   const [toTimePicker, setToTimePicker] = useState(new Date())
   return (
     <Fragment>
-      <Flatpickr className='form-control' value={toTimePicker} onChange={date => setToTimePicker(date)} id='default-picker' style={{backgroundColor: 'white'}}/>
+      <Label>{props.title}</Label>
+      <Flatpickr
+        className="form-control"
+        value={toTimePicker}
+        onChange={(date) => setToTimePicker(date)}
+        id="default-picker"
+        style={{ backgroundColor: 'rgb(241,243,245)', color: 'rgb(190,194,201)' }}
+      />
     </Fragment>
   )
 }
